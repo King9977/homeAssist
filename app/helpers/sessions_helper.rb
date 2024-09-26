262aa8c28cf89ccc067963601ed9e41f08fb9ba4
PC 110 <PC 110@DESKTOP-V53T156.ictw.local>
2024-09-26 13:32:44 +0200

    Konventionen beachten (Clean Code) Formatierung

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,16 +1,13 @@
 module SessionsHelper
 
-  # Methode, um den aktuell eingeloggten Benutzer zu erhalten
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-  # Methode, um zu überprüfen, ob ein Benutzer eingeloggt ist
   def logged_in?
     !current_user.nil?
   end
 
-  # Methode zum Abmelden des Benutzers
   def log_out
     session.delete(:user_id)
     @current_user = nil
